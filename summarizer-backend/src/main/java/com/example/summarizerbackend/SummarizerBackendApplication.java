dd01443a11d769950012e5021a1bb57d71cc715c
Madhav Singh <2025ca93005@wilp.bits-pilani.ac.in>
2026-06-07 23:46:10 +0530

    backend code

---
+++ summarizer-backend/src/main/java/com/example/summarizerbackend/SummarizerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.summarizerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummarizerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SummarizerBackendApplication.class, args);
+    }
+
+}
